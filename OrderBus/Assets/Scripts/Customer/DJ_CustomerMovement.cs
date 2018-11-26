cccf22d7cc8dbf9b01336b253fea6a81b4721819
Vychron <21598@ma-web.nl>
2018-11-26 21:45:54 +0100

    The movement script of a customer, determines how to move around depending on the customer's current state

---
+++ OrderBus/Assets/Scripts/Customer/DJ_CustomerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DJ_CustomerMovement : MonoBehaviour
+{
+    public Vector2 TargetPosition { get; set; }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, 0.002f);
+    }
+
+    public void Execute(CustomerStates state)
+    {
+        StartCoroutine(state.ToString());
+    }
+
+    IEnumerator Wandering()
+    {
+        TargetPosition = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+        yield return null;
+    }
+
+    IEnumerator Approaching()
+    {
+        TargetPosition = new Vector2(1, 0);
+        yield return null;
+    }
+
+    IEnumerator Queueing()
+    {
+        yield return null;
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return null;
+    }
+
+    IEnumerator Leaving()
+    {
+        yield return null;
+    }
+}
