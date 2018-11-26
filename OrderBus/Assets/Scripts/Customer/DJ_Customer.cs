cd07a628e0a19df889e8d107cbf7f9e9b2e3d296
Vychron <21598@ma-web.nl>
2018-11-26 21:44:47 +0100

    The base class for the customer, contains the customer states and a function to pass the states to the movement script of the customer

---
+++ OrderBus/Assets/Scripts/Customer/DJ_Customer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CustomerStates
+{
+    Wandering,
+    Approaching,
+    Queueing,
+    Waiting,
+    Leaving
+}
+
+public class DJ_Customer : MonoBehaviour
+{
+    [SerializeField] private float nextActionTimer;
+
+    private DJ_CustomerMovement movement;
+    [SerializeField] private CustomerStates currentCustomerState = CustomerStates.Wandering;
+
+    void Start()
+    {
+        movement = GetComponent<DJ_CustomerMovement>();
+        StartCoroutine("NextAction");
+    }
+
+    public void SetState(CustomerStates state)
+    {
+        currentCustomerState = state;
+    }
+
+    IEnumerator NextAction()
+    {
+        if (Random.Range(0f, 1f) > 0.85 && currentCustomerState == CustomerStates.Wandering)
+        {
+            currentCustomerState = CustomerStates.Approaching;
+        }
+        movement.Execute(currentCustomerState);
+        yield return new WaitForSeconds(nextActionTimer);
+        StartCoroutine("NextAction");
+        yield return null;
+    }
+}
