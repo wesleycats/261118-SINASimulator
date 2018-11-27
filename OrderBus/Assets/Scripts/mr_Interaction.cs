6139decf150df251b8b8ac223c4ba8d1dab20d71
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-11-27 13:49:29 +0100

    adds interaction script (BoxCast)

---
+++ OrderBus/Assets/Scripts/mr_Interaction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mr_Interaction : MonoBehaviour {
+
+    private float castRange;
+    private float castSpeed;
+    private bool castHitDetect;
+
+    Collider castCollider;
+    RaycastHit castHit;
+
+
+    void Start () {
+        castCollider = GetComponent<Collider>();
+        castRange = 10f;
+        castSpeed = 20f;
+	}
+	
+	void FixedUpdate () {
+        castHitDetect = Physics.BoxCast(castCollider.bounds.center, transform.localScale, transform.forward, out castHit, transform.rotation, castRange);
+        if (castHitDetect)
+        {
+            Debug.Log("Hit : " + castHit.collider.name);
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        if (castHitDetect)
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * castRange);
+            Gizmos.DrawWireCube(transform.position + transform.forward * castRange, transform.localScale);
+        } else
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * castRange);
+            Gizmos.DrawWireCube(transform.position + transform.forward * castRange, transform.localScale);
+        }
+    }
+}
