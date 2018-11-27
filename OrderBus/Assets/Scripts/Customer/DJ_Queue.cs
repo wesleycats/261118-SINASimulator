70f375ac908476e020d9f48d41dec719ad291586
Vychron <21598@ma-web.nl>
2018-11-27 19:32:34 +0100

    Added functionality to remove customers from the queue, and improved lining up the customers

--- OrderBus/Assets/Scripts/Customer/DJ_Queue.cs
+++ OrderBus/Assets/Scripts/Customer/DJ_Queue.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DJ_Queue : MonoBehaviour
 {
+    public delegate void NewCustomer();
+    public event NewCustomer NextCustomer;
+
     private enum Side
     {
         Left = -1,
@@ -14,26 +18,61 @@ public class DJ_Queue : MonoBehaviour
     [SerializeField] private Side side;
     [Header("Finetuner for the positions of the customers in the queue")]
     [SerializeField] private float offset;
-    private List<GameObject> customers = new List<GameObject>();
+    [Header("Maximum length of the queue")]
+    [SerializeField] private int maxLength;
+    [SerializeField] private List<GameObject> customers = new List<GameObject>();
+    [SerializeField] private List<GameObject> list = new List<GameObject>();
 
-    void OnTriggerEnter2D(Collider2D other)
+    public GameObject GetCustomer()
+    {
+        if (customers.ElementAtOrDefault(0) != null)
+        {
+            return customers[0];
+        }
+        return null;
+    }
+
+    void RemoveCustomer()
+    {
+        customers.Remove(customers[0]);
+        StartCoroutine("SetCustomerPositions");
+    }
+
+    IEnumerator SetCustomerPositions()
+    {
+        DJ_CustomerMovement m;
+        int l = customers.Count;
+        for (int i = 0; i < l; i++)
+        {
+            m = customers[i].GetComponent<DJ_CustomerMovement>();
+            m.QueuePosition = i;
+            m.TargetPosition = new Vector2((i + offset) * side.GetHashCode() * 0.2f, 0);
+        }
+        yield return null;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
     {
         GameObject o = other.gameObject;
-        
         if (o.tag == "Customer" && !customers.Contains(o))
         {
             DJ_Customer c = o.GetComponent<DJ_Customer>();
             DJ_CustomerMovement m = o.GetComponent<DJ_CustomerMovement>();
-            if (customers.Count < 4)
+            if (customers.Count < maxLength)
             {
-                print("Colliding");
+                c.Leaving += RemoveCustomer;
                 customers.Add(o);
-                m.TargetPosition = new Vector2((customers.IndexOf(o) + offset) * side.GetHashCode() * 0.2f, 0);
+                m.EndOfQueue = new Vector2(offset * side.GetHashCode() * 0.2f, 0);
+                StartCoroutine("SetCustomerPositions");
                 c.SetState(CustomerStates.Queueing);
+                if (NextCustomer != null)
+                {
+                    NextCustomer();
+                }
             }
             else
             {             
-                m.TargetPosition = new Vector2((1 + offset) * side.GetHashCode(), 0);
+                m.TargetPosition = new Vector2((1.2f + offset) * side.GetHashCode(), 0);
                 m.StopAllCoroutines();
                 c.SetState(CustomerStates.Wandering);
             }
