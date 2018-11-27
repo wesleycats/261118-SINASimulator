cd0656fedcf41cde0d66cb73c2573a9fd0399cad
DimitrivanManen <17382@ma-web.nl>
2018-11-27 19:41:49 +0100

    Finished Display Item it's currently with a temp character class

---
+++ OrderBus/Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {coffee, thea, blanket, none}
+public class Item : MonoBehaviour {
+
+	private ItemType item;
+
+	public Item(ItemType itemType){
+		item = itemType;
+	}
+
+	public ItemType GetItem{ get { return item; } set { item = value; } }
+	
+}
