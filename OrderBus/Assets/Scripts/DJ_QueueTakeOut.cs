294d353be49e4dc1c5d3d7841d3eb02a16f7c556
Vychron <21598@ma-web.nl>
2018-11-27 19:34:24 +0100

    Created a script to aid the queue, which guides the customer through ordering and leaving

---
+++ OrderBus/Assets/Scripts/DJ_QueueTakeOut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DJ_QueueTakeOut : MonoBehaviour
+{
+    private DJ_Queue queue;
+    private GameObject customer;
+    DJ_Customer c;
+    DJ_CustomerMovement m;
+
+    void Start()
+    {
+        queue = GetComponent<DJ_Queue>();
+        queue.NextCustomer += GetNextCustomer;
+    }
+
+    void GetNextCustomer()
+    {       
+
+        customer = queue.GetCustomer();
+        c = customer.GetComponent<DJ_Customer>();
+        m = customer.GetComponent<DJ_CustomerMovement>();
+        m.InPosition -= Order;
+        m.InPosition += Order;
+    }
+
+    void Order()
+    {
+        m.InPosition -= Order;
+        c.PlaceOrder();
+    }
+}
