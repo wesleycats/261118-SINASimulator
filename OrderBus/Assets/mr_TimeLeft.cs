35226ae52ec5bdca840e02c1754b6624b5bd192d
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-11-29 12:14:05 +0100

    Add's PassingTime Script (Timeleft from 100 to 0 in seconds)

---
+++ OrderBus/Assets/mr_TimeLeft.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class mr_TimeLeft : MonoBehaviour {
+
+
+    private bool isTimeUp = false;
+
+    private TimeDisplay timeDisplay;
+
+    // Time in seconds
+    private float timeLimit = 120f;
+    private float timeLeft = 120f;
+    private float timeProgress = 100;
+
+
+    private void Start()
+    {
+        timeDisplay = GameObject.FindObjectOfType<TimeDisplay>();
+    }
+
+    void Update () {
+        if (!isTimeUp)
+        {
+            if(timeLeft < 0)
+            {
+                Debug.Log("Time's up!!!!");
+                isTimeUp = true;
+                Time.timeScale = 0;
+            }
+
+
+            timeProgress -= Time.deltaTime * (100 / timeLimit);
+
+            timeLeft -= Time.deltaTime;
+
+            if (timeProgress > 0)
+            timeDisplay.UpdateDisplay(timeProgress);
+        }
+    }
+}
