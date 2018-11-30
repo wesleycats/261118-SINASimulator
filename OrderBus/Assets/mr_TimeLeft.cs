46c908f8ce4f0324a22e34995610848faf4c8a74
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-11-30 15:24:56 +0100

    Add's win condition

--- OrderBus/Assets/mr_TimeLeft.cs
+++ OrderBus/Assets/mr_TimeLeft.cs
@@ -3,32 +3,45 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class mr_TimeLeft : MonoBehaviour {
+public class mr_TimeLeft : MonoBehaviour
+{
 
 
     private bool isTimeUp = false;
 
-    private TimeDisplay timeDisplay;
+    // Take juda's script to pass the timeProgress variable to
+    //private TimeDisplay timeDisplay;
+
+        private mr_TriggerWin winTrigger;
 
     // Time in seconds
     private float timeLimit = 120f;
-    private float timeLeft = 120f;
+    private float timeLeft;
     private float timeProgress = 100;
 
 
     private void Start()
     {
-        timeDisplay = GameObject.FindObjectOfType<TimeDisplay>();
+        // Find the scripts
+        //timeDisplay = GameObject.FindObjectOfType<TimeDisplay>();
+        winTrigger = GameObject.FindObjectOfType<mr_TriggerWin>();
+
+        // Log time and progress
+        //InvokeRepeating("TimeLogging", 0f, 1f);
+
+        timeLeft = timeLimit;
     }
 
-    void Update () {
+    void FixedUpdate()
+    {
         if (!isTimeUp)
         {
-            if(timeLeft < 0)
+            if (timeLeft < 0)
             {
                 Debug.Log("Time's up!!!!");
                 isTimeUp = true;
                 Time.timeScale = 0;
+                return;
             }
 
 
@@ -37,7 +50,23 @@ public class mr_TimeLeft : MonoBehaviour {
             timeLeft -= Time.deltaTime;
 
             if (timeProgress > 0)
-            timeDisplay.UpdateDisplay(timeProgress);
+            {
+                // pass time progresstion
+                //timeDisplay.UpdateDisplay(timeProgress);
+
+            } else
+            {
+                winTrigger.TriggerWin();
+                // Trigger Win Condition
+            }
         }
+
     }
+
+    void TimeLogging()
+    {
+        Debug.Log("Time left : " + Mathf.Floor(timeLeft) + " seconds");
+        Debug.Log("Progress : " + Mathf.Floor(timeProgress) + "%");
+    }
+
 }
