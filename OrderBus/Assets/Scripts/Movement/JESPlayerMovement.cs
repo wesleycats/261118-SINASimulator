f4b04ea0b9783472d20e623d50abaec72c802494
Fahdon <thejes@hotmail.nl>
2018-11-30 12:19:30 +0100

    Push for the playeble demo
    Player is able to move. Some change is required to make it feel good,

---
+++ OrderBus/Assets/Scripts/Movement/JESPlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JESPlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private JESPlayerInput playerInput;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float friction;
+	private Vector3 movement;
+	
+
+	private void Awake()
+	{
+		playerInput.OnInput += Move;
+	}
+
+	private void Move(InputActions inputAction, float axisAmount)	{
+		if (inputAction == InputActions.Down || inputAction == InputActions.Up) {
+			movement.y = axisAmount;
+		} else if (inputAction == InputActions.Left || inputAction == InputActions.Right) {
+			movement.x = axisAmount;
+		}
+	}
+
+	private void Update()
+	{
+		Debug.Log(movement);
+		movement *= friction * speed * Time.deltaTime;
+		this.transform.position += movement;
+	}
+}
