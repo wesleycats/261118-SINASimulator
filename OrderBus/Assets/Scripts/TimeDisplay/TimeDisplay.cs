d6685b9361ddc1d295a765eaef394d2d00c7c777
JudaHensen <24311@ma-web.nl>
2018-11-30 09:15:07 +0100

    Added Time display
    - added canvas elements
    - added 1 script
    - added temporal art
    - added prefab

---
+++ OrderBus/Assets/Scripts/TimeDisplay/TimeDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDisplay : MonoBehaviour {
+
+    private Image image;
+
+	void Awake () {
+        image = GetComponent<Image>();
+        image.fillAmount = 0;
+	}
+
+    public void UpdateDisplay(float percentage)
+    {
+        percentage /= 100;
+        if (percentage < 0) percentage = 0;
+        else if (percentage > 1) percentage = 1;
+        image.fillAmount = percentage;
+    }
+}
