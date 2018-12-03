855673a7674eeece540e686bdbcbab339ed4fa3d
JudaHensen <24311@ma-web.nl>
2018-12-03 15:45:54 +0100

    Added Pause Menu:
    - Added PauseMenu script
    - Added new Canvas elements
    - Main Menu scene not yet assigned to the PauseMenu script

---
+++ OrderBus/Assets/Scripts/UserInterface/PauseMenu/JH_PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JH_PauseMenu : MonoBehaviour {
+
+    [SerializeField]
+    private Scene _mainMenu;
+    private bool gamePaused = false;
+    public GameObject pauseMenu;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused) Resume();
+            else Pause();
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(_mainMenu.name);
+    }
+
+    public bool GetGamePaused() { return gamePaused; }
+}
