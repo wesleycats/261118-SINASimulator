7e0b6020cb9b3ee916b9f5514063341dedb73d96
Vychron <21598@ma-web.nl>
2018-12-04 13:49:33 +0100

    Expanded Item class
    - Added a time int along with a getter

--- OrderBus/Assets/Scripts/Item.cs
+++ OrderBus/Assets/Scripts/Item.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType {coffee, thea, choco, sandwhich, cookie, pie, blanket, diaper, poncho, none}
+public enum ItemType {Coffee, Tea, Choco, Sandwitch, Cookie, Cake, Blanket, Diaper, Poncho, None}
 public class Item {
 
 	private ItemType item;
+    private int time;
 
-	public Item(ItemType itemType){
+	public Item(ItemType itemType, int waitTime){
 		item = itemType;
+        time = waitTime;
 	}
 
 	public ItemType GetItem{ get { return item; } }
+    public int GetTime { get { return time; } }
 	
 }
