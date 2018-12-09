ccc02b881fafec6d5982f7a625142454c5cd2ff0
Wesley Cats <23973@ma-web.nl>
2018-12-09 15:25:14 +0100

    Added main menu scene

---
+++ OrderBus/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Credits()
+	{
+		//SceneManager.LoadScene(2);
+		//Debug.Log("Go to credits scene");
+	}
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+			//Application.OpenURL(webplayerQuitURL);
+#else
+			Application.Quit();
+#endif
+	}
+}
