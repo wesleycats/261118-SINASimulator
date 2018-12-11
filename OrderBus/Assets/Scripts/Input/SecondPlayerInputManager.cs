a1954e40838e0f97823f1214383b37333aa9ebe0
Fahdon <thejes@hotmail.nl>
2018-12-11 14:03:55 +0100

    InputPackage
    Added a debug way to recieve input from other players

---
+++ OrderBus/Assets/Scripts/Input/SecondPlayerInputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondPlayerInputManager : InputManager {
+
+	public static SecondPlayerInputManager instance;
+
+	// Use this for initialization
+	void Awake () {
+		instance = this;
+
+		OnInput += DebugInput;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		
+	}
+
+	public void SendInput(InputActions inputAction, float axis){
+		if (OnInput != null)
+		{
+			OnInput(inputAction,axis);
+		}
+	}
+
+	private void DebugInput(InputActions inputActions, float axisNumber)
+	{
+		Debug.Log("Input: " + inputActions + " Axis: " + axisNumber);
+	}
+}
