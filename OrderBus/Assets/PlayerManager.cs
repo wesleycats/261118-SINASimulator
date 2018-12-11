e7cdba80ae0c1cabad2102549534d80e74a44ab0
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-12-11 14:16:09 +0100

    Adds player spawning

---
+++ OrderBus/Assets/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager instance;
+
+    public GameObject player;
+
+    public List<GameObject> playersList;
+
+    private void Start()
+    {
+        playersList = new List<GameObject>();
+        instance = this;
+    }
+
+    public void InstansiatePlayer (Vector3 position, long objectId) {
+
+        GameObject playerObject = Instantiate(player);
+
+        RemotePlayer remotePlayer = playerObject.GetComponent<RemotePlayer>();
+
+        remotePlayer.objectId = objectId;
+
+        playersList.Add(playerObject);
+
+	}
+
+    public void RemovePlayer()
+    {
+
+    }
+}
---
+++ OrderBus/Assets/RemotePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemotePlayer : MonoBehaviour {
+
+    public long objectId;
+
+    private void OnDestroy()
+    {
+        
+    }
+}
